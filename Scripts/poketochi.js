5ab501f5532f88e93761e10c564ace6c7343a171
andresgamo <andresgarciamolina3@gmail.com>
2023-07-19 17:20:24 -0500

    adding pokedex display type backwards and endGame funtion stable version

--- Scripts/poketochi.js
+++ Scripts/poketochi.js
@@ -1,9 +1,9 @@
 const url = "https://pokeapi.co/api/v2/pokemon";
 let actionStatus = false;
 let interval;
-let id = 0;
+let id = JSON.parse(sessionStorage.assets).length - 1;
 
-pokemon = {
+let pokemon = {
   sleep: 100,
   hunger: 100,
   sprint: 0,
@@ -12,7 +12,7 @@ pokemon = {
     if (pokemon.hunger <= 96) {
       pokemon.sleep -= 2;
       pokemon.hunger += 4;
-    }else{
+    } else {
       pokemon.hunger = 100;
       stop();
     }
@@ -21,7 +21,7 @@ pokemon = {
     if (pokemon.sleep <= 98) {
       pokemon.sleep += 2;
       pokemon.hunger -= 1;
-    }else{
+    } else {
       pokemon.sleep = 100;
       stop();
     }
@@ -34,8 +34,14 @@ pokemon = {
       pokemon.seniority += 1;
     }
   },
-  alive: function(){
-    return pokemon.sleep > 0 && pokemon.hunger > 0 ?  true: false;
+  alive: function () {
+    return pokemon.sleep > 0 && pokemon.hunger > 0 ? true : false;
+  },
+  init: function () {
+    this.sleep = 100;
+    this.hunger = 100;
+    this.sprint = 0;
+    this.seniority = 0;
   },
 };
 
@@ -52,19 +58,25 @@ function statsUpdate(){
 
 function endGame(){
   stop();
-  pokemon.sleep < 0 ? pokemon.sleep = 0 : pokemon.hunger = 0;
+  pokemon.sleep < 0 ? (pokemon.sleep = 0) : (pokemon.hunger = 0);
   statsUpdate();
   const actions = document.querySelector(".buttons-container");
   for (const action of actions.childNodes) {
     action.removeEventListener("click", play);
-  };
+  }
+  const screen = document.querySelector(".middle");
+  screen.innerHTML = `  <div class="txt-screen">
+                          <p class="txt-bold">Game Over!</p>
+                        </div>
+                        <div class="icon-container">
+                          <i class="fa-solid fa-bed fa-lg"></i>
+                          <i class="fa-solid fa-laptop fa-lg"></i>
+                          <i class="fa-solid fa-utensils fa-lg"></i>
+                        </div>`;
 }
 
-function initListeners() {
+function initPoketochiListeners() {
   const actions = document.querySelector(".buttons-container");
-  const changeCharacterSelector = document.querySelector('.button-selectors');
-
-  changeCharacterSelector.addEventListener('click', changeCharacter);
 
   for (const action of actions.childNodes) {
     if (actionStatus) {
@@ -77,25 +89,31 @@ function initListeners() {
   }
 }
 
+function initPokedexListeners(){
+  const changeCharacterSelector = document.querySelector('.button-selectors');
+  changeCharacterSelector.addEventListener('click', changeCharacter);
+}
+
 function changeCharacter(evt){
   const assetsQuatinty = JSON.parse(sessionStorage.assets).length;
   const selector = evt.target.attributes.value.textContent;
-  if(selector == 'back' && id > 0){
-    id -= 1;
-  }else if(selector == 'next' && id < (assetsQuatinty-1)){
+  if(selector == 'back' && id < (assetsQuatinty-1)){
     id += 1;
+  }else if(selector == 'next' && id > 0){
+    id -= 1;
   }else if(selector == 'enter'){
-    updateCharacter(id);
+    initPoketochi(id);
   }
   updateCharacterContainer(id);
 }
 
-function updateCharacterContainer(id = 0){
+function updateCharacterContainer(){
   const assets = JSON.parse(sessionStorage.assets);
   const name = document.querySelector('.character-name');
   const img = document.querySelector('.img-pokemon');
   img.src = assets[id].img;
   name.textContent = assets[id].name;
+  
 }
 
 function updateAction(action, actionStatus){
@@ -120,14 +138,14 @@ function play(evt) {
   const action = evt.target.attributes.value.textContent;
   actionStatus = true;
   updateAction(action, actionStatus);
-  initListeners();
+  initPoketochiListeners();
   interval = setInterval(() => actions(action), 1000);
 }
 
 function stop() {
   actionStatus = false;
   updateAction( null , actionStatus);
-  initListeners();
+  initPoketochiListeners();
   clearInterval(interval);
   interval = null;
 }
@@ -136,22 +154,22 @@ function showPokedex(){
   const pokedex = document.querySelector('.pokedex-section');
   pokedex.innerHTML = 
                         `<div class="pokedex">
-                        <div class="character-container">
-                            <img class="img-fit img-pokemon" src="./assets/QMark.png" alt="pokemon image">
-                            <div class="character-name">Who's that Pokémon?</div>
-                        </div>
-                        <div class="button-selectors">
-                            <div class="btn-left prev-character" value="back">
-                                <i class="fa fa-backward txt-light" value="back"></i>
-                            </div>
-                            <div class="btn-enter">
-                            <img class="img-fit" value="enter" src="./assets/Poké_Ball_icon.svg.png" alt="pokeball">
-                            </div>
-                            <div class="btn-right next-character" value="next">
-                                <i class="fa fa-forward txt-bold" value="next"></i>
-                            </div>
-                        </div>
-                      </div>`
+                          <div class="character-container">
+                              <img class="img-fit img-pokemon" src="./assets/QMark.png" alt="pokemon image">
+                              <div class="character-name">Who's that Pokémon?</div>
+                          </div>
+                          <div class="button-selectors">
+                              <div class="btn-left prev-character" value="back">
+                                  <i class="fa fa-backward txt-light" value="back"></i>
+                              </div>
+                              <div class="btn-enter">
+                              <img class="img-fit" value="enter" src="./assets/Poké_Ball_icon.svg.png" alt="pokeball">
+                              </div>
+                              <div class="btn-right next-character" value="next">
+                                  <i class="fa fa-forward txt-bold" value="next"></i>
+                              </div>
+                          </div>
+                        </div>`
 }
 
 function actions(action) {
@@ -167,15 +185,31 @@ function actions(action) {
 
 async function updateCharacter(id = 0) {
   const assets = JSON.parse(sessionStorage.assets);
-  const pokemonGif = document.querySelector(".gif-pokemon");
+  const screen = document.querySelector('.middle');
+  screen.innerHTML = 
+                      ` <img class="gif-pokemon" src="" alt="pokemon gif">
+                        <div class="icon-container">
+                          <i class="fa-solid fa-bed fa-lg"></i>
+                          <i class="fa-solid fa-laptop fa-lg"></i>
+                          <i class="fa-solid fa-utensils fa-lg"></i>
+                        </div>`;
+  
+  const pokemonGif = document.querySelector('.gif-pokemon');
   pokemonGif.src = assets[id].gif;
+}
+
+function initPoketochi(id){
+  updateCharacter(id);
+  initPoketochiListeners();
+  pokemon.init();
+  statsUpdate()
 }
 
 (async () => {
   if (sessionStorage.assets) {
     showPokedex();
     updateCharacterContainer();
-    updateCharacter();
+    initPokedexListeners()
   } else {
     const screen = document.querySelector(".middle");
     screen.innerHTML = 
@@ -183,5 +217,5 @@ async function updateCharacter(id = 0) {
     <div class="txt-bold">Go catch them all!</div>
     <a href="./marketPlace.html" class="pulse-link">BuyCards</a>`;
   }
-  initListeners();
+  
 })();
